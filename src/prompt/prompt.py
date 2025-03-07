399d69c426a421fa0b7c9b84d8a97756a46d7cd0
Pedro Cunha <pedrocunha.swe@gmail.com>
2025-03-07 20:46:08 -0300

    feat: add zeroshot prompt

--- src/prompt/prompt.py
+++ src/prompt/prompt.py
@@ -15,8 +15,8 @@ class PromptInterface(ABC):
     def save_final_answer(self, answer) -> None:
         pass
 
-
-class PromptFewShot(PromptInterface):
+# there is no example in this prompt
+class PromptZeroShot(PromptInterface):
     def __init__(self, title: str, question: str, code: str) -> None:
         self.title = title
         self.main_question = f"{question}\n{code}"
@@ -47,3 +47,34 @@ class PromptFewShot(PromptInterface):
     def get_final_answer(self) -> str | None:
         return self.final_answer
 
+# there is an example in this prompt
+class PromptFewShot(PromptInterface):
+    def __init__(self, title: str, question: str, code: str, example: str) -> None:
+        self.title = title
+        self.main_question = f"{question}\n{example}\n{code}"
+        self.answers = []
+        self.final_answer = None
+
+    def save_intermediate_answer(self, answer, error) -> None:
+        self.answers.append({"answer": answer, "error": error})
+
+    def save_final_answer(self, answer) -> None:
+        self.save_intermediate_answer(answer, None)
+        self.final_answer = answer
+
+    def get_prompt(self) -> str:
+        question = f"question: {self.main_question}"
+        iterations_logs = ""
+        for iteration in self.answers:
+            answer, error = iteration["answer"], iteration["error"]
+            follow_up = f"this answer: {answer} got this error {error}"
+            iterations_logs = f"{iterations_logs}\n{follow_up}"
+
+        prompt = f"{question}\n{iterations_logs}"
+        return prompt
+
+    def get_answers(self) -> List:
+        return self.answers
+
+    def get_final_answer(self) -> str | None:
+        return self.final_answer
