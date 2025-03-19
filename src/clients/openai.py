cd3e3df991b3c0fb82d5b319e39301d95ad9b69a
JoaoGMSF <jgmsf@cin.ufpe.br>
2025-03-19 00:19:56 -0300

    fix: increase maximum retry attempts and update prompt class structure

--- src/clients/openai.py
+++ src/clients/openai.py
@@ -9,38 +9,38 @@ class OpenAIClient:
     def __init__(self, logger: logging.Logger = None):
         self.client = OpenAI()
         self.default_output_format = '{"code": "#include <iostream>\n\nint main() {\n    std::cout << "Hello, World!" << std::endl;\n    return 0;\n}"}'
-        self.model = "gpt-4"
         self.messages = (
             []
         )  # to append messages and generate an message history between the user and the AI
         self.logger = logger
 
     def interact(self, message: str):
-        for attempt in range(MAX_RETRY_PER_REQUEST):
-            self.logger.info(f"Attempt: {attempt}")
-            try:
-                response = self.client.chat.completions.create(
-                    model="gpt-4.5-preview",
-                    messages=[{"role": "user", "content": message}],
-                    tools=[
-                        {
-                            "type": "function",
-                            "function": {
-                                "name": "generate_code",
-                                "description": "Generate C++ source code in JSON format.",
-                                "parameters": DEFAULT_JSON_PATTERN,
-                                "strict": True,
-                            },
-                        }
-                    ],
-                    tool_choice="required",
-                )
+        try:
+            response = self.client.chat.completions.create(
+                model="gpt-4o",
+                messages=message,
                 
-                args = response.choices[0].message.tool_calls[0].function.arguments
+                tools=[
+                    {
+                        "type": "function",
+                        "function": {
+                            "name": "generate_code",
+                            "description": "Generate C++ source code in JSON format.",
+                            "parameters": DEFAULT_JSON_PATTERN,
+                            "strict": True,
+                        },
+                    }
+                ],
+                tool_choice="required",
+                temperature=0.6,
+                top_p=0.7,
+            )
+            
+            args = response.choices[0].message.tool_calls[0].function.arguments
 
-                return args
-            except openai.error.RateLimitError as e:
-                self.logger.error("Rate limit exceeded")
-                time.sleep(RETRY_DELAY)
-            except Exception as e:
-                raise errors.RequestFailedError(str(e))
+            return args
+        except openai.RateLimitError as e:
+            self.logger.error("Rate limit exceeded")
+            time.sleep(RETRY_DELAY)
+        except Exception as e:
+            raise errors.RequestFailedError(str(e))
--- src/prompt/prompt.py
+++ src/prompt/prompt.py
@@ -1,31 +1,12 @@
-from abc import ABC, abstractmethod
 from typing import List
 
-class PromptInterface(ABC):
-    @abstractmethod
-    def get_prompt(self) -> str:
-        pass
-
-    @abstractmethod
-    def save_intermediate_answer(self, answer, error) -> None:
-        pass
-
-    @abstractmethod
-    def save_final_answer(self, answer) -> None:
-        pass
-
-class Prompt(PromptInterface):
+class Prompt():
     def __init__(self, question: str, code: str) -> None:
         self.main_question = f"{question}\n{code}"
-        self.answers = []
         self.final_answer = None
         self.messages = [{"role": "user", "content": self.main_question}]
 
-    def save_intermediate_answer(self, answer, error) -> None:
-        self.answers.append({"answer": answer, "error": error})
-
     def save_final_answer(self, answer) -> None:
-        self.save_intermediate_answer(answer, None)
         self.final_answer = answer
 
     def add_output(self,output):
@@ -34,20 +15,8 @@ class Prompt(PromptInterface):
     def add_output_error(self,outputError):
         self.messages.append({"role": "user", "content": outputError})
 
-    def get_prompt(self) -> str:
+    def get_messages(self) -> str:
         return self.messages
-        # question = f"question: {self.main_question}"
-        # iterations_logs = ""
-        # for iteration in self.answers:
-        #     answer, error = iteration["answer"], iteration["error"]
-        #     follow_up = f"this answer: {answer} got this error {error}"
-        #     iterations_logs = f"{iterations_logs}\n{follow_up}"
-
-        # prompt = f"{question}\n{iterations_logs}"
-        #return prompt
-
-    def get_answers(self) -> List:
-        return self.answers
 
     def get_final_answer(self) -> str | None:
         return self.final_answer
